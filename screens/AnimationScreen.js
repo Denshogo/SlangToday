50eb238a458cfb416e39878d35bf111b38e139e8
Denshogo <denshogo125@gmail.com>
2025-06-03 00:07:54 +0900

    Initialize Expo SDK 53 skeleton

---
+++ screens/AnimationScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function AnimationScreen() {
+  return (
+    <View>
+      <Text>TODO</Text>
+    </View>
+  );
+}
